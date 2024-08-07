59342082c3c64cad1488aa8edda1f09227e8de02
lims62 <lims62@meicloud.com>
2024-08-07 14:47:37 +0800

    修正ReadReg.bat

--- ReadReg.bat
+++ ReadReg.bat
@@ -1,19 +1,10 @@
 @echo off
 
-:::::::::::::::::::::::::::::::::::::::::::
-::输入1	set REG_PATH=HKEY_LOCAL_MACHINE\SOFTWARE\DefaultUserEnvironment
-::输入2	set REG_KEY=Path
-::调用	call ReadReg.bat
-::输出	%REG_VALUE%
-
-::或
-
-:: call ReadReg.bat RETURN_VALUE HKEY_LOCAL_MACHINE\SOFTWARE\DefaultUserEnvironment Path
-:: echo %REG_VALUE%
-:::::::::::::::::::::::::::::::::::::::::::
-
 ::第一个参数为返回值变量名（不带%）
 set RETURN_NAME=%1
+if "%RETURN_NAME%"=="" (
+	set RETURN_NAME=REG_VALUE
+)
 ::第二个参数为注册表路径
 if "%REG_PATH%"=="" (
 	set REG_PATH=%2
@@ -23,9 +14,10 @@ if "%REG_KEY%"=="" (
 	set REG_KEY=%3
 )
 
-if "%RETURN_NAME%"=="" (
-	set RETURN_NAME=REG_VALUE
-)
+echo return_name=%RETURN_NAME%
+echo reg_path=%REG_PATH%
+echo reg_key=%REG_KEY%
+
 set %RETURN_NAME%=
 if "%REG_PATH%"=="" (
 	goto err
@@ -34,13 +26,13 @@ if "%REG_KEY%"=="" (
 	goto err
 )
 
-set REG_VALUE=
 for /f "tokens=2*" %%a in ('reg query "%REG_PATH%" /v "%REG_KEY%"') do set "READ_VALUE=%%b"
 set %RETURN_NAME%=%READ_VALUE%
-::echo The value of "%REG_KEY%" is "%REG_VALUE%"
+::echo The value of "%REG_KEY%" is "%RETURN_NAME%"
 
 goto done
 :err
+echo 错误，用法参考：
 echo 1.
 echo set REG_PATH=HKEY_LOCAL_MACHINE\SOFTWARE\DefaultUserEnvironment
 echo set REG_KEY=Path
@@ -60,7 +52,3 @@ echo.
 echo 如果路径有空格，则需要使用1或2
 
 :done
-set REG_PATH=
-set REG_KEY=
-set RETURN_NAME=
-set REG_VALUE=
